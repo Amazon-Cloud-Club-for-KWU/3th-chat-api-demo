e429d70d5e81e2178d8ed3ecdaeb4732670ed531
강지훈 <clone0922@gmail.com>
2025-08-24 19:30:15 +0900

    refactor

--- src/main/java/com/acc/chatdemo/common/swagger/SwaggerConfig.java
+++ src/main/java/com/acc/chatdemo/common/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.acc.chatdemo.config;
+package com.acc.chatdemo.common.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
--- src/main/java/com/acc/chatdemo/redis/RedisConfig.java
+++ src/main/java/com/acc/chatdemo/redis/RedisConfig.java
@@ -1,15 +1,12 @@
-package com.acc.chatdemo.config;
+package com.acc.chatdemo.redis;
 
-import com.acc.chatdemo.redis.RedisMessageListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
